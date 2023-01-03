4ac0f2f9b5fd384622064653c14a9e4f78585b11
DMacMakes <56147357+DMacMakes@users.noreply.github.com>
2023-01-03 16:37:34 +1100

    Added Action enum using code from Microsoft. DisplayAction is separate function. Only display things or send radio if user input changes.

--- main.ts
+++ main.ts
@@ -1,10 +1,54 @@
+enum Actions {
+    Forward,
+    Backward,
+    Left,
+    Right,
+    Pinch,
+    Unpinch,
+    Stop
+}
 enum Flags {
     A = 1 << 0
 }
-let action = ""
+function displayAction (anAction: number) {
+    if (anAction == Actions.Pinch) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            . # # # .
+            . # . # .
+            . # . # .
+            `)
+    } else if (anAction == Actions.Unpinch) {
+        basic.showLeds(`
+            # . . . #
+            # . . . #
+            # . . . #
+            . # . # .
+            . . # . .
+            `)
+    } else if (anAction == Actions.Left) {
+        basic.showArrow(ArrowNames.West)
+    } else if (anAction == Actions.Right) {
+        basic.showArrow(ArrowNames.East)
+    } else if (anAction == Actions.Forward) {
+        basic.showArrow(ArrowNames.North)
+    } else if (anAction == Actions.Backward) {
+        basic.showArrow(ArrowNames.South)
+    } else if (anAction == Actions.Stop) {
+        basic.showIcon(IconNames.SmallDiamond)
+    } else {
+    	
+    }
+}
+let input_action = 0
 let roll = 0
 let pitch = 0
+let action = 0
+let action_last_frame = Actions.Stop
+displayAction(Actions.Stop)
 radio.setGroup(1)
+radio.sendNumber(action_last_frame)
 let LEFT_ROLL = -15
 let RIGHT_ROLL = 15
 let FORWARD_PITCH = 15
@@ -16,38 +60,24 @@ basic.forever(function () {
     roll = input.rotation(Rotation.Roll)
     // close claw
     if (input.buttonIsPressed(Button.A)) {
-        basic.showLeds(`
-            # . . . #
-            # . . . #
-            # . . . #
-            . # . # .
-            . . # . .
-            `)
-        action = "unpinch"
+        input_action = Actions.Unpinch
     } else if (input.buttonIsPressed(Button.B)) {
-        basic.showLeds(`
-            . . # . .
-            . . # . .
-            . # # # .
-            . # . # .
-            . # . # .
-            `)
-        action = "pinch"
+        input_action = Actions.Pinch
     } else if (roll < LEFT_ROLL) {
-        basic.showArrow(ArrowNames.West)
-        action = "left"
+        input_action = Actions.Left
     } else if (roll > RIGHT_ROLL) {
-        basic.showArrow(ArrowNames.East)
-        action = "right"
+        input_action = Actions.Right
     } else if (pitch < FORWARD_PITCH) {
-        basic.showArrow(ArrowNames.North)
-        action = "forward"
+        input_action = Actions.Forward
     } else if (pitch > BACKWARD_PITCH) {
-        basic.showArrow(ArrowNames.South)
-        action = "backward"
+        input_action = Actions.Backward
     } else {
-        basic.showIcon(IconNames.SmallDiamond)
-        action = "stop"
+        input_action = Actions.Stop
     }
-    radio.sendString(action)
+    // If the user input has changed to something new, act on it.
+    if (input_action != action_last_frame) {
+        displayAction(input_action)
+        radio.sendNumber(input_action)
+        action_last_frame = input_action
+    }
 })
