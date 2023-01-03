eb987091ab8c4b88752f66cce8a8873b54a5a77e
DMacMakes <56147357+DMacMakes@users.noreply.github.com>
2023-01-03 15:59:37 +1100

    about to add radio comms

--- main.ts
+++ main.ts
@@ -1,46 +1,53 @@
 enum Flags {
     A = 1 << 0
 }
-let pitch = 0
+let action = ""
 let roll = 0
+let pitch = 0
 radio.setGroup(1)
-let LEFT_ROLL = -20
-let RIGHT_ROLL = 20
-let FORWARD_PITCH = 20
-let BACKWARD_PITCH = 45
+let LEFT_ROLL = -15
+let RIGHT_ROLL = 15
+let FORWARD_PITCH = 15
+let BACKWARD_PITCH = 50
 // display image
 // open claw
 basic.forever(function () {
+    pitch = input.rotation(Rotation.Pitch)
+    roll = input.rotation(Rotation.Roll)
     // close claw
     if (input.buttonIsPressed(Button.A)) {
         basic.showLeds(`
-            . . . . .
-            . # . # .
+            # . . . #
+            # . . . #
             # . . . #
             . # . # .
-            . . . . .
+            . . # . .
             `)
+        action = "unpinch"
     } else if (input.buttonIsPressed(Button.B)) {
         basic.showLeds(`
-            . . . . .
-            # . . . #
+            . . # . .
+            . . # . .
+            . # # # .
             . # . # .
-            # . . . #
-            . . . . .
+            . # . # .
             `)
+        action = "pinch"
     } else if (roll < LEFT_ROLL) {
-        basic.showArrow(ArrowNames.East)
-        roll = input.rotation(Rotation.Roll)
-    } else if (roll > RIGHT_ROLL) {
         basic.showArrow(ArrowNames.West)
-        roll = input.rotation(Rotation.Roll)
+        action = "left"
+    } else if (roll > RIGHT_ROLL) {
+        basic.showArrow(ArrowNames.East)
+        action = "right"
     } else if (pitch < FORWARD_PITCH) {
         basic.showArrow(ArrowNames.North)
-        pitch = input.rotation(Rotation.Pitch)
+        action = "forward"
     } else if (pitch > BACKWARD_PITCH) {
         basic.showArrow(ArrowNames.South)
-        pitch = input.rotation(Rotation.Pitch)
+        action = "backward"
     } else {
         basic.showIcon(IconNames.SmallDiamond)
+        action = "stop"
     }
+    radio.sendString(action)
 })
